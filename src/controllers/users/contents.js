32006b538a36d368f232dfdd813b94608a96b763
Lautaro Jayat <lautaro.jayat@gmail.com>
2020-01-06 20:21:38 -0300

    Many Changes in content's controllers.js
    +Added a middleware in order to check/populate the cache from the content's routes
    +Fixed all confilcts with the renders
    +Added headers objects to the handlers
    +Now we send an object and not an array with an object when we send content
    +Added a regexp to delete html tags from the og:description field for the meta tag

--- src/controllers/users/contents.js
+++ src/controllers/users/contents.js
@@ -2,38 +2,141 @@ const News = require('../../models/news');
 const Emitions = require('../../models/emitions');
 const Columns = require('../../models/column');
 const HOME_CACHE = require('../../cache/HOME_CACHE');
+const cache_functions = require('../../cache/cache_functions');
 const contents_ctrl = {};
 
+function makeOGdescriptionsSafeAgain(description) {
+    let safe = description.replace(/<\/?\w*\W*>/gm, "");
+    return safe
+}
+
+contents_ctrl.get_cache = async (req, res, next) => {
+    if (HOME_CACHE.news.length < 1) {
+        await cache_functions.mainFunction();
+        return next();
+    }
+    else {
+        next();
+    }
+}
+
 contents_ctrl.get_new = async (req, res) => {
+    let headers;
+    const _news = HOME_CACHE.news;
     const url = req.params.url;
-    if (HOME_CACHE.columns.filter(element => element.url === url)) {
-        console.log("works")
-        res.send(HOME_CACHE.columns.filter(element => element.url === url))
+    if (_news.filter(element => element.url === "mi-url-de-noticia").length > 0) {
+        let news = _news.filter(element => element.url === url);
+        news = news[0];
+        headers = {
+            pageTitle: news.title + " | Radio Entre-Piernas",
+            ogDescription: makeOGdescriptionsSafeAgain(news.description),
+            ogTitle: "Radio Entre-Piernas",
+            ogImage: news.thumbnail,
+            news: true,
+            altFormat: {
+                altCSS: ['<link rel="stylesheet" href="/content.css">'],
+                altScripts: []
+            }
+        };
+        //console.log('from cache works?');
+        //console.log(news);
+        res.render("contents", { news, headers })
     } else {
-        const columns = await News.find({ url: url }, { _id: 0 });
-        res.send(columns);
+        let news = await News.find({ url: url }, { _id: 0 });
+        news = news[0];
+        headers = {
+            pageTitle: news.title + " | Radio Entre-Piernas",
+            ogDescription: makeOGdescriptionsSafeAgain(news.description),
+            ogTitle: news.title + " | Noticias | Radio Entre-Piernas",
+            ogImage: news.thumbnail,
+            news: true,
+            altFormat: {
+                altCSS: ['<link rel="stylesheet" href="/content.css">'],
+                altScripts: []
+            }
+        };
+        //console.log(news);
+        //console.log("from database works?");
+        res.render("contents", { news, headers });
     }
 }
 
 contents_ctrl.get_column = async (req, res) => {
+    let headers;
     const url = req.params.url;
-    if (HOME_CACHE.columns.filter(element => element.url === url)) {
-        //console.log("works")
-        res.send(HOME_CACHE.columns.filter(element => element.url === url))
+    const _columns = HOME_CACHE.columns;
+    //const REDUCED_CACHE
+    if (_columns.filter(element => element.url === "mi-url-de-noticia").length) {
+        let columns = _columns.filter(element => element.url === url);
+        columns = columns[0];
+        headers = {
+            pageTitle: columns.title + " | Radio Entre-Piernas",
+            ogDescription: columns.description,
+            ogTitle: "Radio Entre-Piernas",
+            ogImage: columns.thumbnail,
+            profile: false,
+            altFormat: {
+                altCSS: ['<link rel="stylesheet" href="/content.css">'],
+                altScripts: []
+            }
+        };
+        //console.log("columns works!");
+        res.render("contents", { columns, headers })
     } else {
-        const columns = await Columns.find({ url: url }, { _id: 0 });
-        res.send(columns);
+        let columns = await Columns.find({ url: url }, { _id: 0 });
+        columns = columns[0];
+        headers = {
+            pageTitle: columns.title + " | Radio Entre-Piernas",
+            ogDescription: columns.description,
+            ogTitle: "Radio Entre-Piernas",
+            ogImage: columns.thumbnail,
+            profile: false,
+            altFormat: {
+                altCSS: ['<link rel="stylesheet" href="/content.css">'],
+                altScripts: []
+            }
+        };
+        //console.log("columns works!");
+        res.render("contents", { columns, headers });
     }
 }
 contents_ctrl.get_emition = async (req, res) => {
+    let headers;
     const url = req.params.url;
-    if (HOME_CACHE.emitions.filter(element => element.url === url)) {
-        //console.log("works")
-        res.send(HOME_CACHE.emitions.filter(element => element.url === url))
+    const _emitions = HOME_CACHE.emitions;
+    if (_emitions.filter(element => element.url === "mi-url-de-noticia").length) {
+        let emitions = _emitions.filter(element => element.url === url);
+        emitions = emitions[0];
+        headers = {
+            pageTitle: emitions.title + " | Radio Entre-Piernas",
+            ogDescription: emitions.description,
+            ogTitle: "Radio Entre-Piernas",
+            ogImage: emitions.thumbnail,
+            profile: false,
+            altFormat: {
+                altCSS: ['<link rel="stylesheet" href="/content.css">'],
+                altScripts: []
+            }
+        };
+        //console.log("emitions works!");
+        res.render("contents", { emitions })
     } else {
-        const emition = await News.find({ url: url }, { _id: 0 });
-        res.send(emition);
-    };
+        let emitions = await Emitions.find({ url: url }, { _id: 0 });
+        emitions = emitions[0];
+        headers = {
+            pageTitle: emitions.title + " | Radio Entre-Piernas",
+            ogDescription: emitions.description,
+            ogTitle: "Radio Entre-Piernas",
+            ogImage: emitions.thumbnail,
+            profile: false,
+            altFormat: {
+                altCSS: ['<link rel="stylesheet" href="/content.css">'],
+                altScripts: []
+            }
+        };
+        //console.log("emitions works!");
+        res.render("contents", { emitions, headers });
+    }
 }
 
 
@@ -50,9 +153,11 @@ contents_ctrl.get_all_news = async (req, res) => {
         }
     } else {
         const { news } = HOME_CACHE;
+        //console.log(HOME_CACHE);
         res.render("all_news", { news });
     }
 }
+
 contents_ctrl.get_all_emitions = async (req, res) => {
     if (req.query.page) {
         const emitions = await Emitions.find({}, { _id: 0 }).sort({ _id: -1 }).limit(3).skip(parseInt(req.query.page));
@@ -62,13 +167,17 @@ contents_ctrl.get_all_emitions = async (req, res) => {
     } else {
         const { emitions } = HOME_CACHE;
         res.render("all_emitions", { emitions });
-    }}
+    }
+}
 contents_ctrl.get_all_columns = async (req, res) => {
     if (req.query.page) {
         const columns = await Columns.find({}, { _id: 0 }).sort({ _id: -1 }).limit(3).skip(parseInt(req.query.page));
-        console.log(columns)
-        if (columns.length < 1) { res.sendStatus(500) } else {
+        //console.log(columns)
+        if (columns.length < 1) {
+            res.sendStatus(500)
+        } else {
             res.json({ columns });
+
         }
     } else {
         const { columns } = HOME_CACHE;
