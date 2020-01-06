6b23beac33813201bde21e859cd97bd6ffc1a98d
Lautaro Jayat <lautaro.jayat@gmail.com>
2020-01-06 16:22:20 -0300

    Updated contents.js routes
    +Changed the name of the contents_ctl object to impove readability
    +Change routes names in order to avoid routing conflicts with controllers that uses req.params

--- src/routes/contents.js
+++ src/routes/contents.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
-const c_ctrl = require('../controllers/users/contents');
+const contents_ctrl = require('../controllers/users/contents');
 
-router.get('/news/:url', c_ctrl.get_new);
-router.get('/columns/:url', c_ctrl.get_column);
-router.get('/emitions/:url', c_ctrl.get_emition);
-router.get('/all/news', c_ctrl.get_all_news);
-router.get('/all/columns', c_ctrl.get_all_columns);
-router.get('/all/emitions', c_ctrl.get_all_emitions);
+router.get('/contents/news/:url', contents_ctrl.get_new);
+router.get('/contents/columns/:url', contents_ctrl.get_column);
+router.get('/contents/emitions/:url', contents_ctrl.get_emition);
+router.get('/all/news', contents_ctrl.get_all_news);
+router.get('/all/columns', contents_ctrl.get_all_columns);
+router.get('/all/emitions', contents_ctrl.get_all_emitions);
 
 
 module.exports = router;
