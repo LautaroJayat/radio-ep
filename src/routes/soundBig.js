361cb9e1d69f0d93504c2a91ebd349b8b01f8f31
Lautaro Jayat <lautaro.jayat@gmail.com>
2020-01-08 20:11:16 -0300

    Updated SoundBig.js routes/controllers
    +We still have both routes and controllers in the same file
    +Added routes to delete, port and get the iframes for the content's playlist

--- src/routes/soundBig.js
+++ src/routes/soundBig.js
@@ -1,21 +1,44 @@
 const router = require('express').Router();
 const SoundBig = require('../models/soundBig');
-//const Sound = require('../models/sound');
+const cache_functions =require('../cache/cache_functions');
 const { isAuthenticated } = require('../helpers/auth');
+const { safeIframe } = require('../helpers/url');
+const HOME_CACHE = require('../cache/HOME_CACHE');
+const contents_ctrl = require('../controllers/users/contents');
 
-router.put('/admin/bigsound/register', async (req, res) => {
-    const { iframe } = req.body;
-    const registeredIframe = await SoundBig.findOne();
-    if (registeredIframe === null) {
-        const newIframe = new SoundBig({ iframe });
-        await newIframe.save();
-    } else {
-        registeredIframe.iframe = iframe;
-        await registeredIframe.save()
-    }
+
+
+router.delete('/iframes/delete/:_id', async (req, res) => {
+    console.log(req.params._id);
+    await SoundBig.findByIdAndDelete(req.params._id);
+
     res.redirect('/admin/iframes');
+});
+
+router.post('/admin/bigsound/register', async (req, res) => {
+    var { iframe } = req.body;
+    const onlyID = true;
+    let video_id = await safeIframe(iframe, onlyID);
+    console.log(video_id);
+    if (video_id === undefined) {
+        req.flash("error_msg", "Querid@, ingresastemal el iframe");
+        return res.redirect('/admin/iframes')
+    } else {
+        console.log("we are in else");
+        const newIframe = new SoundBig({ video_id });
+        await newIframe.save();
+        await cache_functions.addBigSound(newIframe);
+        return res.redirect('/admin/iframes');
+    }
 
 });
+
+router.get('/superlist', contents_ctrl.get_cache, async (req, res) => {
+    let index = Math.floor(Math.random() *10)
+    console.log(index);
+    iframe = HOME_CACHE.bigSound[index];
+    return res.json({ id: iframe.video_id })
+})
 
 
 module.exports = router;
