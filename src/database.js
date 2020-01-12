54efa928ae06bd6f8e39d75d88dbc94fc21770e9
Lautaro Jayat <lautaro.jayat@gmail.com>
2020-01-12 00:34:43 -0300

    Updated src/database.js
    + There was a deprecation warning after the update of mongoose to 5.8.x
    + Added {useUnifiedTopology: true} to mongoose.connect()
    + The warning was the following:
    
    (node:3453) DeprecationWarning: current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version. To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.

--- src/database.js
+++ src/database.js
@@ -3,7 +3,8 @@ const DB_URI = !process.env.PORT || process.env.NODE_ENV === 'dev' ? 'mongodb://
 mongoose.connect( DB_URI ,{
     useCreateIndex: true,
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 })
 .then(db => console.log('DB is connected'))
 .catch(err => console.log(err));
