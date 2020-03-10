d1c7e52068e0f8904da5ee18f9988fdba19c6a0b
Lautaro Jayat <lautaro.jayat@gmail.com>
2020-03-10 14:16:30 -0300

    Updated database.js
    + Added good practices to database.js
    + Changed some env conditionals to be prepared for a microservices migration

--- src/database.js
+++ src/database.js
@@ -1,10 +1,45 @@
 const mongoose = require('mongoose');
-const DB_URI = !process.env.PORT || process.env.NODE_ENV === 'dev' ? 'mongodb://localhost/radioentrepiernas' : process.env.DB_ATLAS_MONGODB 
-mongoose.connect( DB_URI ,{
+
+function dburi() {
+    if (process.env.ENV === 'test') {
+        return 'radioep-testing'
+    }
+    if (process.env.ENV === 'dev') {
+        return 'radioep-dev'
+    }
+    if (process.env.ENV === 'prod' || !process.env.ENV) {
+        return 'radioentrepiernas'
+    }
+};
+
+const DBURI = dburi();
+
+
+const DB = mongoose.connect('mongodb://localhost/' + DBURI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
-})
-.then(db => console.log('DB is connected'))
-.catch(err => console.log(err));
+}).then(() => {
+    console.log('Everything seems ok in DB')
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected to', DBURI);
+});
+
+mongoose.connection.on('error', () => {
+    console.log('There was an error in Mongoose')
+});
+
+process.on('SIGINT', () => {
+    mongoose.connection.close((err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('Mongoose connection closed due to app termination');
+        process.exit(0);
+    });
+});
+
+module.exports = { DB }
